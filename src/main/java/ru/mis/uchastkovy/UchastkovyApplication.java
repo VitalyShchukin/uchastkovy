b569817cd8d8c494aa14d1fc4adb68c2b08ddad8
VitalyShchukin <shchukin_v_v@mail.ru>
2023-10-07 17:16:45 +0300

    Initial commit

---
+++ src/main/java/ru/mis/uchastkovy/UchastkovyApplication.java
@@ -0,0 +1,13 @@
+package ru.mis.uchastkovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchastkovyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UchastkovyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mis/uchastkovy/UchastkovyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mis.uchastkovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UchastkovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
